b396bc512c137e581e4cbbd22884e16525c83da1
HD-VG <vargashectordanielgonzales@gmail.com>
2025-10-05 10:48:34 -0400

    cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Habilita CORS para permitir requests desde el frontend (temporal para dev)
+  app.enableCors({
+    origin: '*',  // O 'http://localhost:5173' para solo tu Vue app
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
   await app.listen(3000);
 }
 bootstrap();
